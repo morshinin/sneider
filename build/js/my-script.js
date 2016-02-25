c45beafd6a927e4c294350991a4d5de0b3dbc0cd
Mikhail Morshinin <314159265absurdus@gmail.com>
2016-02-25 23:33:06 +0300

    Added some scripts

---
+++ build/js/my-script.js
@@ -0,0 +1,20 @@
+(function() {
+  // var d = Date.today().next().monday();
+  // var d = Date.parse('next monday');
+  var d = Date.parse('next monday');
+  var month = new Array();
+  month[0] = '01';
+  month[1] = '02';
+  month[2] = '03';
+  month[3] = '04';
+  month[4] = '05';
+  month[5] = '06';
+  month[6] = '07';
+  month[7] = '08';
+  month[8] = '09';
+  month[9] = '10';
+  month[10] = '11';
+  month[11] = '12';
+  var n = month[d.getMonth()];
+  document.getElementById('date').innerHTML = d.getDate() + '.' + n + '.' + d.getFullYear() + '!';
+}());
